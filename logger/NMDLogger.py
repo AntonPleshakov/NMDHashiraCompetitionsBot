111e9d0e7952b2b9d34fb2f9869ac3fa91812002
Anton Pleshakov <anton.plieshakov@gmail.com>
2023-08-29 12:19:04 +0200

    Move logger setup to a separate folder

---
+++ logger/NMDLogger.py
@@ -0,0 +1,16 @@
+import logging
+
+from ConfigManager import config, MODE
+from logger.RotatingGDriveHandler import RotatingGDriveHandler
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("NMDHashiraBot")
+
+fmt = config[MODE]["LOG_FORMAT"]
+date_fmt = config[MODE]["LOG_DATE_FORMAT"]
+handler = RotatingGDriveHandler(
+    config[MODE]["LOG_FILE_NAME"], max_bytes=10000, backup_count=10
+)
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter(fmt, date_fmt))
+logger.addHandler(handler)
--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-import logging
 import os
 
 import telebot
 
 from ConfigManager import config, MODE
 from DBManager import DBManager
-from RotatingGDriveHandler import RotatingGDriveHandler
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("NMDHashiraBot")
-
-fmt = config[MODE]["LOG_FORMAT"]
-date_fmt = config[MODE]["LOG_DATE_FORMAT"]
-handler = RotatingGDriveHandler(
-    config[MODE]["LOG_FILE_NAME"], max_bytes=10000, backup_count=10
-)
-handler.setLevel(logging.INFO)
-handler.setFormatter(logging.Formatter(fmt, date_fmt))
-logger.addHandler(handler)
 
 bot = telebot.TeleBot(config[MODE]["TOKEN"])
 
