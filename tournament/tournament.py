0d3d42e5bfffc8226da388ca853ec40434aa5a95
Anton Pleshakov <anton.plieshakov@gmail.com>
2023-10-09 18:04:24 +0200

    Get players from Ratings DB

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -1,6 +1,7 @@
 from enum import Enum
-from typing import List
+from typing import List, Dict
 
+from db.ratings_db import RatingsDB
 from db.tournament_db import TournamentDB
 from match import MatchResult
 from mcmahon_pairing import McMahonPairing
@@ -19,39 +20,48 @@ class Tournament:
         tournament_db = TournamentDB.get_latest_tournament()
         if not tournament_db or tournament_db.is_finished():
             tournament_db = TournamentDB.create_new_tournament()
-        self._db: TournamentDB = tournament_db
+        self._tournament_db: TournamentDB = tournament_db
+        ratings = RatingsDB().get_ratings()
+        self._players: Dict[str, Player] = {
+            player.tg_username: player for player in ratings
+        }
         self._state: TournamentState = (
             TournamentState.IN_PROGRESS
-            if self._db.get_tours_number() > 0
+            if self._tournament_db.get_tours_number() > 0
             else TournamentState.REGISTRATION
         )
 
-        players_list = self._db.get_registered_players()
+        players_list = self._tournament_db.get_registered_players()
         previous_tours = []
-        for i in range(self._db.get_tours_number() - 1):
-            previous_tours.append(self._db.get_results(i))
+        for i in range(self._tournament_db.get_tours_number() - 1):
+            previous_tours.append(self._tournament_db.get_results(i))
         self._pairing: McMahonPairing = McMahonPairing(players_list, previous_tours)
 
     def new_round(self):
         if self._state == TournamentState.FINISHED:
             raise TournamentFinishedError
         self._pairing.update_coefficients(
-            self._db.get_results(), self._db.get_tours_number()
+            self._tournament_db.get_results(), self._tournament_db.get_tours_number()
         )
         pairs = self._pairing.gen_pairs()
-        self._db.start_new_tour(pairs)
+        self._tournament_db.start_new_tour(pairs)
         self._state = TournamentState.IN_PROGRESS
 
-    def add_player(self, player: Player):
+    def add_player(self, player_tg_username: str):
         if self._state != TournamentState.REGISTRATION:
             raise TournamentStartedError
-        self._db.register_player(player)
+        if player_tg_username not in self._players:
+            new_player = Player(player_tg_username)
+            RatingsDB().add_user_rating(new_player)
+            self._players[player_tg_username] = new_player
+        player = self._players[player_tg_username]
+        self._tournament_db.register_player(player)
         self._pairing.add_player(player)
 
     def add_result(self, players_tg_name: List[str], result: MatchResult):
         if len(players_tg_name) != 2:
             raise KeyError("Error: only 2 players in pair accepted")
-        pairs = self._db.get_results()
+        pairs = self._tournament_db.get_results()
         for i, match in enumerate(pairs):
             if match.first_player.tg_username in players_tg_name:
                 if match.second_player.tg_username not in players_tg_name:
@@ -66,10 +76,11 @@ class Tournament:
                         if result == MatchResult.SecondWon
                         else MatchResult.SecondWon
                     )
-                self._db.register_result(i, result)
+                self._tournament_db.register_result(i, result)
                 return
 
     def finish_tournament(self):
         self._pairing.update_coefficients(
-            self._db.get_results(), self._db.get_tours_number()
+            self._tournament_db.get_results(), self._tournament_db.get_tours_number()
         )
+        self._state = TournamentState.FINISHED
