3b9b1a8ca45e621e966e132af157c03719fa2ed3
Anton Pleshakov <anton.plieshakov@gmail.com>
2023-11-17 18:12:03 +0100

    Update update_match_result functions with states

--- tg/tournament/update_match_result.py
+++ tg/tournament/update_match_result.py
@@ -1,9 +1,15 @@
 from telebot import TeleBot
+from telebot.handler_backends import StatesGroup, State
 from telebot.types import CallbackQuery, InlineKeyboardMarkup
 
 from tg.utils import Button, empty_filter
 from tournament.match import Match, MatchResult, MATCH_RESULT_TO_STR
 from tournament.tournament_manager import tournament_manager
+
+
+class UpdateMatchStates(StatesGroup):
+    match_to_update = State()
+    new_result = State()
 
 
 def match_to_string(match: Match):
@@ -23,7 +29,7 @@ def chose_match_to_update(cb_query: CallbackQuery, bot: TeleBot):
     keyboard = InlineKeyboardMarkup(row_width=1)
     for i, match in enumerate(matches):
         text = match_to_string(match)
-        keyboard.add(Button(text, f"tournament/update_match_result/{i}").inline())
+        keyboard.add(Button(text, f"{i}").inline())
     keyboard.add(Button("Назад в Турнир", "tournament").inline())
     bot.edit_message_text(
         text="Выберите матч для редактирования результата",
@@ -31,10 +37,13 @@ def chose_match_to_update(cb_query: CallbackQuery, bot: TeleBot):
         message_id=cb_query.message.id,
         reply_markup=keyboard,
     )
+    bot.set_state(cb_query.from_user.id, UpdateMatchStates.match_to_update)
 
 
 def chose_result_to_update_match(cb_query: CallbackQuery, bot: TeleBot):
-    match_index = int(cb_query.data.split("/")[-1])
+    match_index = int(cb_query.data)
+    bot.add_data(cb_query.from_user.id, match_index=match_index)
+
     match = tournament_manager.tournament.db.get_results()[match_index]
     current_result = match.result
     keyboard = InlineKeyboardMarkup(row_width=1)
@@ -42,7 +51,7 @@ def chose_result_to_update_match(cb_query: CallbackQuery, bot: TeleBot):
         if res == current_result:
             continue
         text = match_to_string(match)
-        keyboard.add(Button(text, cb_query.data + "/" + res.name).inline())
+        keyboard.add(Button(text, res.name).inline())
     keyboard.add(Button("Назад в Турнир", "tournament").inline())
     bot.edit_message_text(
         text="Выберите новый результат",
@@ -50,13 +59,16 @@ def chose_result_to_update_match(cb_query: CallbackQuery, bot: TeleBot):
         message_id=cb_query.message.id,
         reply_markup=keyboard,
     )
+    bot.set_state(cb_query.from_user.id, UpdateMatchStates.new_result)
 
 
 def update_result(cb_query: CallbackQuery, bot: TeleBot):
-    match_index = int(cb_query.data.split("/")[-2])
-    new_result = MatchResult[cb_query.data.split("/")[-1]]
+    with bot.retrieve_data(cb_query.from_user.id) as data:
+        match_index = data["match_index"]
+    new_result = MatchResult[cb_query.data]
     tournament_manager.tournament.db.register_result(match_index, new_result)
     bot.send_message(chat_id=cb_query.message.chat.id, text="Результат успешно изменен")
+    bot.delete_state(cb_query.from_user.id)
 
 
 def register_handlers(bot: TeleBot):
