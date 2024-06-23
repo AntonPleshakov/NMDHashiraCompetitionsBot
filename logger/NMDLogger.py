cd5fc70658b257584b594d998c0e545a614d2a04
Anton Pleshakov <anton.plieshakov@gmail.com>
2024-06-23 13:42:23 +0300

    Update loggers

--- logger/NMDLogger.py
+++ logger/NMDLogger.py
@@ -4,13 +4,25 @@ from config.config import getconf
 from .RotatingGDriveHandler import RotatingGDriveHandler
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("NMDHashiraBot")
 
 fmt = getconf("LOG_FORMAT")
 date_fmt = getconf("LOG_DATE_FORMAT")
-handler = RotatingGDriveHandler(
+
+gdrive_handler = RotatingGDriveHandler(
     getconf("LOG_FILE_NAME"), max_bytes=10000, backup_count=10
 )
-handler.setLevel(logging.INFO)
-handler.setFormatter(logging.Formatter(fmt, date_fmt))
-logger.addHandler(handler)
+gdrive_handler.setLevel(logging.INFO)
+gdrive_handler.setFormatter(logging.Formatter(fmt, date_fmt))
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(logging.Formatter(fmt, date_fmt))
+
+nmd_logger = logging.getLogger("NMDHashiraBot")
+nmd_logger.addHandler(gdrive_handler)
+nmd_logger.addHandler(stream_handler)
+nmd_logger.setLevel(logging.INFO)
+
+bot_logger = logging.getLogger("TeleBot")
+bot_logger.addHandler(gdrive_handler)
+bot_logger.setLevel(logging.INFO)
