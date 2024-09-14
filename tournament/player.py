5433380edcb3775a9f96e0012db7b1c96628c94e
Anton Pleshakov <anton.plieshakov@gmail.com>
2024-09-14 20:42:40 +0300

    Update player structure

--- tournament/player.py
+++ tournament/player.py
@@ -1,29 +1,53 @@
 from math import floor
 from typing import Set, Dict
 
+from db.ratings import Rating
 from db.tournament_structures import RegistrationRow, Result, Match
+from nmd_exceptions import NewPlayerError
 
 
 class Player:
     def __init__(
         self,
         tg_id: int,
-        rating: int = 100,
+        username: str,
+        rating: int,
+        deviation: int,
+        weeks_to_last_tournament: int,
     ):
         self.tg_id: int = tg_id
+        self.username: str = username
         self.rating: int = rating
-        self.opponents: Set[int] = set()
+        self.deviation: int = deviation
+        self.weeks_to_last_tournament: int = weeks_to_last_tournament
+
+        self.opponents: Set[Player] = set()
         self.results: Dict[int, Match.MatchResult] = {}
         self.had_bye: bool = False
         self.mm: int = floor(rating / 100)
         self.sos: float = 0
         self.sodos: float = 0
 
+        # unused, for rating formulas
+        self.handicap: int = 0
+
     @classmethod
-    def from_registration(cls, registration: RegistrationRow):
+    def from_rating(cls, rating: Rating):
+        try:
+            weeks_to_last_tournament = rating.get_weeks()
+        except NewPlayerError:
+            weeks_to_last_tournament = -1
+
+        username = rating.nmd_username.value
+        if not username:
+            username = rating.tg_username.value
+
         player = cls(
-            registration.tg_id.value,
-            registration.rating.value,
+            rating.tg_id.value,
+            username,
+            rating.rating.value,
+            rating.deviation.value,
+            weeks_to_last_tournament,
         )
         return player
 
